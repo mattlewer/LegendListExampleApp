aa9c3b42b58defe34265d6a9f86d11252609ce54
Matt <mlewer@hotmail.co.uk>
2025-04-29 18:02:00 +0100

    Example

--- App.tsx
+++ App.tsx
@@ -1,118 +1,98 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {LegendList} from '@legendapp/list';
+import {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+interface RowItem {
+  name: string;
+  desc: string;
+}
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const rowItems: RowItem[] = [
+  {name: 'Item 1', desc: 'Description for item 1'},
+  {name: 'Item 2', desc: 'Description for item 2'},
+  {name: 'Item 3', desc: 'Description for item 3'},
+  {name: 'Item 4', desc: 'Description for item 4'},
+  {name: 'Item 5', desc: 'Description for item 5'},
+  {name: 'Item 6', desc: 'Description for item 6'},
+  {name: 'Item 7', desc: 'Description for item 7'},
+  {name: 'Item 8', desc: 'Description for item 8'},
+  {name: 'Item 9', desc: 'Description for item 9'},
+  {name: 'Item 10', desc: 'Description for item 10'},
+  {name: 'Item 11', desc: 'Description for item 11'},
+  {name: 'Item 12', desc: 'Description for item 12'},
+  {name: 'Item 13', desc: 'Description for item 13'},
+  {name: 'Item 14', desc: 'Description for item 14'},
+  {name: 'Item 15', desc: 'Description for item 15'},
+  {name: 'Item 16', desc: 'Description for item 16'},
+  {name: 'Item 17', desc: 'Description for item 17'},
+  {name: 'Item 18', desc: 'Description for item 18'},
+  {name: 'Item 19', desc: 'Description for item 19'},
+  {name: 'Item 20', desc: 'Description for item 20'},
+  {name: 'Item 21', desc: 'Description for item 21'},
+  {name: 'Item 22', desc: 'Description for item 22'},
+  {name: 'Item 23', desc: 'Description for item 23'},
+  {name: 'Item 24', desc: 'Description for item 24'},
+  {name: 'Item 25', desc: 'Description for item 25'},
+  {name: 'Item 26', desc: 'Description for item 26'},
+  {name: 'Item 27', desc: 'Description for item 27'},
+  {name: 'Item 28', desc: 'Description for item 28'},
+  {name: 'Item 29', desc: 'Description for item 29'},
+  {name: 'Item 30', desc: 'Description for item 30'},
+];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App(): React.JSX.Element {
+  const [itemsToShow, setItemsToShow] = useState(rowItems);
+  const [inputText, setInputText] = useState('');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const renderItem = (item: RowItem) => {
+    return <RowItemCard item={item} />;
+  };
+
+  const filterItems = (text: string) => {
+    setInputText(text)
+    const newItems = rowItems.filter(item => item.name.includes(text))
+    setItemsToShow(newItems)
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1, backgroundColor: 'white', padding: 8}}>
+    <TextInput value={inputText} onChangeText={(newText) => filterItems(newText)} style={{borderColor: 'black', borderWidth: 1, color: 'black'}}/>
+      <LegendList
+        data={itemsToShow}
+        recycleItems={true}
+        contentContainerStyle={{gap: 8, padding: 8}}
+        renderItem={({item}) => renderItem(item)}
+      />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+
+
+
+interface RowItemProps {
+  item: RowItem;
+}
+const RowItemCard = (props: RowItemProps) => {
+  const colors = [
+    'red',
+    'blue',
+    'orange',
+    'yellow',
+    'green',
+    'pink',
+    'teal',
+    'black',
+    'grey'
+  ];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  return (
+    <View style={{padding: 20, borderRadius: 4, borderColor: 'black', borderWidth: 1, overflow: 'hidden'}}>
+      <View style={{height: 200, backgroundColor: randomColor}} />
+      <Text style={{color: 'black'}}>{props.item.name}</Text>
+      <Text style={{color: 'black'}}>{props.item.desc}</Text>
+    </View>
+  );
+};
